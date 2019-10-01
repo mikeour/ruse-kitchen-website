12c64af63c85a115e8d781dfff009056e1ecf661
Michael Roeslein <michaeljroeslein@gmail.com>
2019-10-01 18:29:19 -0400

    Testing Netlify integration

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,17 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
-import Header from "./header"
-import "./layout.css"
+import { css, Global } from "@emotion/core"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+    query {
+      image: file(relativePath: { eq: "food.webp" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
         }
       }
     }
@@ -25,28 +19,70 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Global
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
+          }
+
+          /* * + * {
+            margin-top: 1rem;
+          } */
+
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-size: 18px;
+            line-height: 1.4;
+          }
+
+          > div {
+            margin-top: 0;
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: #222;
+            line-height: 1.1;
+
+            + * {
+              margin-top: 0.5rem;
+            }
+
+            strong {
+              color: #222;
+            }
+
+            li {
+              margin-top: 0.25rem;
+            }
+          }
+        `}
+      />
+      <header>
+        <nav
+          css={css`
+            display: flex;
+          `}
+        >
+          <ul>
+            <Link to="/about">About</Link>
+          </ul>
+          <ul>
+            <Link to="/contact">Contact</Link>
+          </ul>
+        </nav>
+      </header>
+      <Img fluid={data.image.sharp.fluid} />
+      <main>{children}</main>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
